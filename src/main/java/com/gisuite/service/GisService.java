eb3f3de35409d8ad175a93a459b20ac893ac7e2d
whdghk1907 <whdghk1907@naver.com>
2025-02-04 20:46:51 +0900

    feat: GIS 데이터 처리를 위한 service 패키지 추가
    - GisService 추가 (GIS 데이터 비즈니스 로직 관리)"

--- src/main/java/com/gisuite/service/GisService.java
+++ src/main/java/com/gisuite/service/GisService.java
@@ -1,4 +1,36 @@
 package com.gisuite.service;
 
+import com.gisuite.entity.GisEntity;
+import com.gisuite.repository.GisRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class GisService {
+
+    private final GisRepository gisRepository;
+
+    public GisService(GisRepository gisRepository) {
+        this.gisRepository = gisRepository;
+    }
+
+    public List<GisEntity> getAllGisData() {
+        return gisRepository.findAll();
+    }
+
+    public Optional<GisEntity> getGisDataById(Long id) {
+        return gisRepository.findById(id);
+    }
+
+    @Transactional
+    public GisEntity saveGisData(GisEntity gisEntity) {
+        return gisRepository.save(gisEntity);
+    }
+
+    @Transactional
+    public void deleteGisData(Long id) {
+        gisRepository.deleteById(id);
+    }
 }
