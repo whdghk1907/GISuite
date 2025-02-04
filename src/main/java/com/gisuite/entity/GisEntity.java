83fd7c7f74ea0cf7e88ed2ad73b28e7931fd41b8
whdghk1907 <whdghk1907@naver.com>
2025-02-04 20:46:12 +0900

    feat: GIS 데이터 처리를 위한 entity 패키지 추가
    - GisEntity 추가 (GIS 데이터 저장을 위한 JPA 엔티티)

--- src/main/java/com/gisuite/entity/GisEntity.java
+++ src/main/java/com/gisuite/entity/GisEntity.java
@@ -1,4 +1,52 @@
 package com.gisuite.entity;
 
+import jakarta.persistence.*;
+import org.locationtech.jts.geom.Geometry;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "gis_data")
 public class GisEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "Geometry")
+    private Geometry geometry;
+
+    public GisEntity() {
+    }
+
+    public GisEntity(String name, Geometry geometry) {
+        this.name = name;
+        this.geometry = geometry;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
 }
