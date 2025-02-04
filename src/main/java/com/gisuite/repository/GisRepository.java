7fddabb9c24e7f5aa54eb2f967a3aa0595d77767
whdghk1907 <whdghk1907@naver.com>
2025-02-04 20:46:35 +0900

    feat: GIS 데이터 접근을 위한 repository 패키지 추가 - GisRepository 추가 (GIS 데이터를 JPA로 관리)

--- src/main/java/com/gisuite/repository/GisRepository.java
+++ src/main/java/com/gisuite/repository/GisRepository.java
@@ -1,4 +1,19 @@
 package com.gisuite.repository;
 
-public class GisRepository {
+import com.gisuite.entity.GisEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GisRepository extends JpaRepository<GisEntity, Long> {
+
+    // 특정 좌표 내 데이터 검색 (예제)
+    @Query("SELECT g FROM GisEntity g WHERE ST_Contains(g.geometry, ST_GeomFromText(:wkt, 4326)) = true")
+    List<GisEntity> findWithinGeometry(String wkt);
+
+    // 특정 속성으로 데이터 검색
+    List<GisEntity> findByName(String name);
 }
